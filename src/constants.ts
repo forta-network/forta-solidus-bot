379c3e8fc524c600eca9484e1ebe33e1b758cb7d
Roberto Cantu <Roberto_Cantu@live.com>
2023-08-01 15:18:10 -0500

    Remove unused path; update other path

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,4 @@
-export const FP_DB_URL: string =
-  "https://raw.githubusercontent.com/forta-network/forta-solidus-bot/main/false.positive.database.json";
-
-export const FP_CSV_PATH: string = "false.positives.csv";
+export const FP_CSV_PATH: string = "./false.positives.csv";
 
 export const BOT_ID: string = "0x1ae0e0734a5d2b4ab26b8f63b5c323cceb8ecf9ac16d1276fcb399be0923567a";
 
